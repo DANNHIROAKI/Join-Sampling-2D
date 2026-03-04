4c3481c97c48b699b43a61a4d5b4789eae9a6e6d
DANNHIROAKI <Dann_Hiroaki@ieee.org>
2026-03-04 09:00:18 +0000

    Force sync local directory

---
+++ build/release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhy/PhD/Compare-304/Ver-2026.3.4-Pro
+# Build directory: /home/dhy/PhD/Compare-304/Ver-2026.3.4-Pro/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
